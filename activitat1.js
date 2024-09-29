b225a3875defd7d9485da706851d3a5afb7611b4
Ana Gómez Pastor <165878099+AnaGmzPstr@users.noreply.github.com>
2024-09-29 18:03:50 +0000

    Codi refactoritzat i més net. Falta arreglar l'estil del check quan la tasca està feta

--- activitat1.js
+++ activitat1.js
@@ -84,7 +84,7 @@ function filtrarPositius() {
 //TODO list 
 //Funció que comproba que la tasca es vàlida i que afegeix la tasca
 function afegirElement() {
-    let tarea = document.getElementById('tasca').value;
+    let tarea = document.getElementById('tasca').value.trim();
     console.log(tarea)
     if (tarea == "") alert("Has d'afegir una tasca!")
     else crearTarea(tarea);
@@ -99,21 +99,34 @@ function vaciarLista() {
 //Funció que crea tots els elements de la tasca donat el missatge
 function crearTarea(mensaje) {
     let lista = document.getElementById('lista');
+    
+    let contenedor = crearElementTarea(mensaje);
+    lista.appendChild(contenedor);
+
+    document.getElementById('tasca').value="";
+}
+
+//Funció auxiliar per crear el contenidor de la tasca
+function crearElementTarea(tasca){
+    let contenedor = document.createElement('div');
+    contenedor.className = 'tarea';
+
+    let check = crearCheck();
+    let label = crearLabel(tasca);
+    let eliminarTarea = crearBtnEliminar(contenedor);
+
+    contenedor.appendChild(check);
+    contenedor.appendChild(label);
+    contenedor.appendChild(eliminarTarea);
+
+    return contenedor
+}
+
+//Funció que crea l'element checkbox
+function crearCheck(){
     let check = document.createElement('input');
-    let eliminarTarea = document.createElement('button');
-    eliminarTarea.textContent = 'X';
-    eliminarTarea.className = 'eliminar';
     check.type = 'checkbox';
-    let label = document.createElement('label');
-    let li = document.createElement('div');
-    li.className = 'tarea';
-    li.appendChild(check);
-    li.appendChild(label);
-    li.appendChild(eliminarTarea);
-    label.textContent = mensaje;
-    lista.appendChild(li);
-    document.getElementById('tasca').value = "";
-    check.addEventListener('change', () => {
+    check.addEventListener('change', () =>{
         if (check.checked) {
             label.style.textDecoration = 'line-through';
             label.style.color='grey';
@@ -121,8 +134,34 @@ function crearTarea(mensaje) {
             label.style.textDecoration = 'none';
             label.style.color='white';
         }
-    })
-    eliminarTarea.addEventListener('click', () => {
-        li.remove();
-    })
+    });
+
+    return check;
 }
+
+//Funció que crea un label
+function crearLabel(tasca){
+    let label = document.createElement('label');
+    label.textContent=tasca;
+    return label;
+}
+
+//Funció que crea el botó que elimina la tasca en concret
+function crearBtnEliminar(contenedor){
+    let boton = document.createElement('button');
+    boton.textContent='X';
+    boton.className='eliminar';
+
+    //Afegim un event que elimina la tasca
+    boton.addEventListener('click', () =>{
+        contenedor.remove();
+    });
+    return boton;
+}
+
+//Afegim un event que accepti la tasca amb la tecla 'enter'
+const inputTasca = document.getElementById('tasca');
+
+inputTasca.addEventListener('keydown', (event)=>{
+    if (event.key ==='Enter') afegirElement();
+})
